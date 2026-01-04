6c626dcd4e7e81cc905b778ac51728133821baa9
Elahe Vahdati <66535264+Elahe-Vahdati@users.noreply.github.com>
2026-01-04 07:27:38 -1200

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,13 +1,28 @@
-package se.fifth.universityPortal;
+package se.fifth.universityPortal.src.main.java.university;
 
 public class Professor extends User {
 
     private String code;
     private String expert;
-
+    
+    public Professor() {
+        
+    }
     public Professor(String name, String username, String password, String code, String expert) {
         super(name, username, password);
         this.code = code;
+        this.expert = expert;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getExpert() {
+        return expert;
+    }
+    public void setExpert(String expert) {
         this.expert = expert;
     }
 
