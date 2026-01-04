2334686115efd23ca25d849ec8bebe55cdaab7b4
Elahe Vahdati <66535264+Elahe-Vahdati@users.noreply.github.com>
2026-01-04 07:19:06 -1200

    Update UserApp.java

--- UserApp.java
+++ UserApp.java
@@ -1,9 +1,10 @@
-package se.fifth.universityPortal;
+package se.fifth.universityPortal.src.main.java.university;
+import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class UserApp {
-    ArrayList<User> users = new ArrayList<>();
+    List<User> users = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
 
     public UserApp() {
@@ -25,45 +26,26 @@ public class UserApp {
         int choice = sc.nextInt();
         switch (choice) {
             case 1->{
-                System.out.println("Enter Student Name:");
-                String name = sc.next();
-                sc.nextLine();
-                System.out.println("Enter Student username:");
-                String username = sc.next();
-                sc.nextLine();
-                System.out.println("Enter Student password:");
-                String password = sc.next();
-                sc.nextLine();
+                User user = userEntery();
                 System.out.println("Enter Student entryYear:");
                 int entryYear = sc.nextInt();
                 sc.nextLine();
                 System.out.println("Enter Student avg:");
                 double avg = sc.nextDouble();
                 sc.nextLine();
-                users.add(new Student(name, username, password, entryYear, avg));
+                users.add(new Student(user.getName(), user.getUsername(), user.getPassword(), entryYear, avg));
                 System.out.println("Student added successfully");
                 break;
             }
             case 2->{
-
-                System.out.println("Enter Professor Name:");
-                String name = sc.next();
-                sc.nextLine();
-                System.out.println("Enter Professor username:");
-                String username = sc.next();
-                sc.nextLine();
-                System.out.println("Enter Professor password:");
-                String password = sc.next();
-                sc.nextLine();
+                User user = userEntery();
                 System.out.println("Enter Professor Code:");
                 String code = sc.next();
                 sc.nextLine();
                 System.out.println("Enter Professor expert:");
                 String expert = sc.next();
                 sc.nextLine();
-
-
-                users.add(new Professor(name, username, password, code, expert));
+                users.add(new Professor(user.getName(), user.getUsername(), user.getPassword(), code, expert));
                 System.out.println("Professor added successfully");
                 break;
             }
@@ -87,7 +69,19 @@ public class UserApp {
 
     }
 
+private User userEntery(){
 
+    System.out.println("Enter Student Name:");
+    String name = sc.next();
+    sc.nextLine();
+    System.out.println("Enter Student username:");
+    String username = sc.next();
+    sc.nextLine();
+    System.out.println("Enter Student password:");
+    String password = sc.next();
+    sc.nextLine();
+    return new User(name, username, password);
+}
 
 
 }
