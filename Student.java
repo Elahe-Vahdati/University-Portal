e66d36bef070f13a1cd482f5ca620c2ce7e89188
Elahe Vahdati <66535264+Elahe-Vahdati@users.noreply.github.com>
2026-01-04 07:28:10 -1200

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,17 +1,30 @@
-package se.fifth.universityPortal;
+package se.fifth.universityPortal.src.main.java.university;
 
 public class Student extends User {
 
     private int entryYear;
     private double avg;
 
-    public Student(){
-        
-    }
+public Student() {
 
+}
     public Student(String name, String username, String password, int entryYear, double avg) {
         super(name, username, password);
         this.entryYear = entryYear;
+        this.avg = avg;
+    }
+    public int getEntryYear() {
+        return entryYear;
+    }
+    
+    public void setEntryYear(int entryYear) {
+        this.entryYear = entryYear;
+    }
+    
+    public double getAvg() {
+        return avg;
+    }
+    public void setAvg(double avg) {
         this.avg = avg;
     }
 
